5d8db1ea0eac7b3b8ea608f005956df63e5dd50b
NguyenVanNam121204 <nt0143436946@gmail.com>
2026-01-07 09:38:44 +0700

    link-cloudfare

--- src/Services/BaseURL.jsx
+++ src/Services/BaseURL.jsx
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://defend-clothing-lamps-routine.trycloudflare.com/api";
+export const API_BASE_URL = "https://blend-basin-clusters-regularly.trycloudflare.com/api";
 export const AUTH_REFRESH_URL = `${API_BASE_URL}/auth/refresh-token`;
